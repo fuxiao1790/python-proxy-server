d2b8fb539a1e784c5d97859b2cc7987ac249d34f
user name <xiao.fu1790@gmail.com>
2022-04-09 13:53:28 -0400

    added mail client

---
+++ mail-client.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.message import EmailMessage
+
+SMTP_SERVER_ADDR = "smtp.gmail.com"
+SMTP_SERVER_PORT = 587
+SENDER_EMAIL = ""
+SENDER_PASSWORD = ""
+RECEIVER_EMAIL = ""
+
+msg = EmailMessage()
+msg["From"] = SENDER_EMAIL
+msg["To"] = RECEIVER_EMAIL
+msg["Subject"] = "Test"
+msg.set_content = "THIS IS A TEST"
+
+client = smtplib.SMTP(SMTP_SERVER_ADDR, SMTP_SERVER_PORT)
+client.ehlo()
+client.starttls()
+client.login(SENDER_EMAIL, SENDER_PASSWORD)
+
+client.send_message(msg)
+client.quit()
